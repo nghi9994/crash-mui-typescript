61a9039ab28414d9306071860fde1b484f5c61ae
nguyenthanhnghi9994@gmail.com <nghi.nguyen@evehr.com>
2022-10-29 22:25:48 +0700

    Feat: icon button - Comment & create icon as button component

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Button, Stack } from '@mui/material'
+import { Button, Stack, IconButton } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
 
 const MuiButton = () => {
   return (
+    /***
+     * Stack display elements as flex column by default
+     * Use IconButton to make icon as button
+     */
     <Stack spacing={4}>
       <Stack direction='row' spacing={2}>
         <Button variant='text'>Text</Button>
@@ -21,6 +26,13 @@ const MuiButton = () => {
         <Button variant='contained' size='small'>small</Button>
         <Button variant='contained' size='medium'>medium</Button>
         <Button variant='contained' size='large'>large</Button>
+      </Stack>
+      <Stack direction='row' spacing={2}>
+        <Button variant='contained' startIcon={<SendIcon />} disableRipple>Send</Button>
+        <Button variant='contained' endIcon={<SendIcon />} disableElevation>Send</Button>
+        <IconButton aria-label='send' color='error' onClick={() => alert('Click')}>
+          <SendIcon />
+        </IconButton>
       </Stack>
     </Stack>
   )
